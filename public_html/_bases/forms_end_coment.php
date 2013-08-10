214e3de3b58a300b49cae3aebbba50231bf5cc31
jpasosa <jpasosa@gmail.com>
2013-08-10 13:43:38 -0300

    bod_entrada_mercaderia_coment :: cuando cierra el ultimo, hace un update sobre los cambios que hubo hacia pro_productos  OKA

--- public_html/_bases/forms_end_coment.php
+++ public_html/_bases/forms_end_coment.php
@@ -1,15 +1,39 @@
 <?php
 
-class forms_end_coment extends FormCommon {
-    
-    //  Cierra el mantenimiento ya cargado, poniendo el campo activo en 1.
-    //  IN:     (0->id_tabla)
-    public  function update_cierre ($valores=NULL){
-        return "
-            UPDATE adm_ytd_mantenimiento_recordatorio
-            SET activo = 1
-            WHERE id_adm_ytd_mantenimiento_recordatorio = $valores[0];
-        ";
-    }
+class forms_end_coment extends FormCommon
+{
+
+
+	//  Cierra el mantenimiento ya cargado, poniendo el campo activo en 1.
+	//  IN:     (0->id_tabla)
+	public  function update_cierre ($valores=NULL) {
+		return "
+		UPDATE adm_ytd_mantenimiento_recordatorio
+		SET activo = 1
+		WHERE id_adm_ytd_mantenimiento_recordatorio = $valores[0];
+		";
+	}
+
+	//  Hace el update sobre pro_productos, cuando cierra ultimo paso de bod_entrada_mercaderia. . . .
+	//  IN:     (0->$prod['id_pro_productos']  |  1->$prod['id_pro_marca']  |  2->$prod['id_pro_subfamilia']  |  3->$prod['productos_por_caja_bem']  |  4->$prod['alto_bem']
+	// 			5->$prod['ancho_bem']  |  6->$prod['fondo_bem']  |  7->$prod['volumen_bem']  |  8->$prod['peso_bem']  |  9->$prod['precio_bem']  |  10->$prod['foto_bem'])
+	public  function update_productos ($valores=NULL) {
+		return "
+		UPDATE pro_productos
+		SET 	id_pro_marca 		= $valores[1],
+			 	id_pro_subfamilia 	= $valores[2],
+			 	productos_por_caja = $valores[3],
+			 	alto 				= $valores[4],
+			 	ancho 				= $valores[5],
+			 	fondo 				= $valores[6],
+			 	volumen 			= $valores[7],
+			 	peso 				= $valores[8],
+			 	precio 				= $valores[9],
+			 	archivo 				= '$valores[10]'
+		WHERE id_pro_productos = $valores[0]
+		;
+		";
+	}
+
 
 }
--- public_html/_php/forms_end_coment.php
+++ public_html/_php/forms_end_coment.php
@@ -1,7 +1,7 @@
 <?php
 
 // Obtengo el archivo de donde fue llamado.
-$exception = new Exception; 
+$exception = new Exception;
 $trace = $exception->getTrace();
 $file = $trace[0]['file'];
 $file = explode('/', $file);
@@ -23,7 +23,7 @@ if(!stristr($pr_proceso, '_recordatorio') === FALSE) { // si tiene _recordatorio
 if(isset($no_comments) && !$no_comments) {
     $all_comments = ProcessComments::getAll($pr_proceso, $id_tabla_proc, 'n');
     $flash_error = Common::setErrorMessage($all_comments); // Si tuviera error, lo carga en $flash_error para mostrar.
-    $tpl->asignar('all_comments', $all_comments);    
+    $tpl->asignar('all_comments', $all_comments);
 }
 
 
@@ -46,6 +46,22 @@ if(isset($_POST['comentario']) && $_POST['comentario'] != '' &&
     if(isset($_POST['solicitar_correccion'])) {
         $accion = 'correccion';
     }
+
+    if($pr_proceso == 'bod_entrada_mercaderia') { // Va a tener que controlar si es el ultimo proceso, va a hacer UPADATE sobre PRO_PRODUCTOS
+        $is_last = Process::isLastFlDias($pr_proceso, $id_tabla_proc );
+        if($is_last) {
+            $all_reg = Process::getTablaSec($pr_proceso, 'prod', $id_tabla_proc, 's');
+            foreach($all_reg AS $prod) {  // debe hacer el update sobre tabla PRO_PRODUCTOS
+                $update_productos = BDConsulta::consulta('update_productos',
+                                                array
+                                                (
+                                                $prod['id_pro_productos'], $prod['id_pro_marca'],$prod['id_pro_subfamilia'],$prod['productos_por_caja_bem'],$prod['alto_bem'],
+                                                $prod['ancho_bem'],$prod['fondo_bem'],$prod['volumen_bem'],$prod['peso_bem'],$prod['precio_bem'], $prod['foto_bem']
+                                                ), 'n');
+            }
+        }
+    }
+
     $comentario = ProcessSends::toNextProcess($pr_proceso, $id_user, $id_tabla_proc, $comment, $accion, 's');
     if($comentario['error'] == false) {
         header('Location: /enviado.html');
@@ -58,7 +74,7 @@ if(isset($_POST['cerrar_mant'])) {
     $update_cierre = BDConsulta::consulta('update_cierre', array($_POST['id_tabla']), 'n');
     if(!is_null($update_cierre)) {
         header('Location: /mant_cerrado.html');
-        exit();    
+        exit();
     }else{
         $flash_error = 'No pudo hacer el cierre del mantenimiento cargado.';
     }
